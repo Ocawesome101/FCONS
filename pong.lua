65d66ef09f12c337cc00b234dd11f12c28b85338
ocawesome101 <ocawesome101@gmail.com>
2021-03-13 20:06:30 -0500

    worked on pong, fixed one or two bugs and added the same number of features

--- pong.lua
+++ pong.lua
@@ -3,19 +3,32 @@
 local w, h = fcons.term.size()
 
 local paddle1, paddle2 = h // 2, h // 2
+local bx, by = 10, 10
 
 local sprites = {
   paddle = fcons.sprite(1, 5),
   background = fcons.sprite(w, h)
 }
 
-gpu.setActiveBuffer(sprites.paddle)
-gpu.setBackground(0xFFFFFF)
-gpu.fill(1, 1, 1, 5, " ")
+fcons.setSprite(sprites.paddle)
+fcons.term.bg(0xFFFFFF)
+fcons.term.size() -- update term size
+fcons.term.clear()
+fcons.setSprite(0)
+fcons.term.size()
 
 local function redraw()
   fcons.drawSprite(sprites.background, 1, 1)
   fcons.drawSprite(sprites.paddle, 1, paddle1)
   fcons.drawSprite(sprites.paddle, w, paddle2)
-  gpu.set(bx, by, "o")
+  fcons.term.fg(0xFFFFFF)
+  fcons.term.cursor(bx, by)
+  fcons.term.write("o")
+end
+
+fcons.term.write("Press Q to quit.\n\nPress any key to begin.\n\n_")
+
+while true do
+  redraw()
+  computer.pullSignal()
 end
